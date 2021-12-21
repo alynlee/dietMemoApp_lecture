0a893bb689bed396b2d4d2bc2b4fb2aedd0bafc4
areum.lee <areum.lee@lge.com>
2021-12-22 07:14:20 +0900

    Make Authenticate with Firebase using Password-Based Accounts

--- app/src/main/java/com/androidlearning/dietmemoapp/MainActivity.kt
+++ app/src/main/java/com/androidlearning/dietmemoapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -19,20 +20,48 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        val btn = findViewById<Button>(R.id.noEmailLoginBtn)
-        btn.setOnClickListener{
-            auth.signInAnonymously()
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener{
+            val email = findViewById<EditText>(R.id.emailArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        val user = auth.currentUser
-                        Log.d("MainActivity", user!!.uid)
+                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(baseContext, "Authentication failed.",
+                        if(password.text.toString().length < 6) {
+                            Toast.makeText(this, "비밀번호는 6자리 이상",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                        Toast.makeText(this, "실패",
                             Toast.LENGTH_SHORT).show()
                     }
                 }
+
+
+        }
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener{
+            val email = findViewById<EditText>(R.id.emailArea)
+            val password = findViewById<EditText>(R.id.passwordArea)
+
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "실패",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener{
+            Firebase.auth.signOut()
+            Toast.makeText(this, "로그아웃 완료", Toast.LENGTH_LONG).show()
         }
     }
 }
