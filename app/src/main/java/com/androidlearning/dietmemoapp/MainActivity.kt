6bd6c524c394b4053b16b17a45fb48e69d9439df
alyn <howerll12@gmail.com>
2021-12-23 22:11:17 +0900

    making layout

--- app/src/main/java/com/androidlearning/dietmemoapp/MainActivity.kt
+++ app/src/main/java/com/androidlearning/dietmemoapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -18,50 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = Firebase.auth
-
-        val joinBtn = findViewById<Button>(R.id.joinBtn)
-        joinBtn.setOnClickListener{
-            val email = findViewById<EditText>(R.id.emailArea)
-            val password = findViewById<EditText>(R.id.passwordArea)
-
-            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
-                    } else {
-                        if(password.text.toString().length < 6) {
-                            Toast.makeText(this, "비밀번호는 6자리 이상",
-                                Toast.LENGTH_SHORT).show()
-                        }
-                        Toast.makeText(this, "실패",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
-
-
-        }
-
-        val loginBtn = findViewById<Button>(R.id.loginBtn)
-        loginBtn.setOnClickListener{
-            val email = findViewById<EditText>(R.id.emailArea)
-            val password = findViewById<EditText>(R.id.passwordArea)
-
-            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
-                    } else {
-                        Toast.makeText(this, "실패",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
-        }
-
-        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
-        logoutBtn.setOnClickListener{
-            Firebase.auth.signOut()
-            Toast.makeText(this, "로그아웃 완료", Toast.LENGTH_LONG).show()
-        }
+        val writeButton = findViewById<ImageView>(R.id.writeBtn)
+        writeButton
     }
 }
--- app/src/main/java/com/androidlearning/dietmemoapp/SplashActivity.kt
+++ app/src/main/java/com/androidlearning/dietmemoapp/SplashActivity.kt
@@ -4,15 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.lang.Exception
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+        auth = Firebase.auth
+
+        try {
+            Log.d("SPLASH", auth.currentUser!!.uid)
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        } catch (e:Exception){
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Handler().postDelayed({
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }, 3000)
+                    } else {
+                        // If sign in fails, display a message to the user.
+
+                    }
+                }
+        }
     }
 }
